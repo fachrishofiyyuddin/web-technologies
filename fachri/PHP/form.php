a6f1c50fba8ca7d08fd877ccf61ac3694c057778
Fahri <37017392+fachrishofiyyuddin@users.noreply.github.com>
2025-01-26 16:04:38 +0700

    commit

---
+++ fachri/PHP/form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Form</title>
+</head>
+
+<body>
+    <h1>Kontak Saya</h1>
+
+    <p>Untuk menghubungi saya, hubungi pesan melalui form dibawah</p>
+
+    <form>
+        <label>Nama</label>
+        <input type="text"><br>
+
+        <label>Email</label>
+        <input type="email"><br>
+
+        <label>Pesan</label>
+        <textarea></textarea><br>
+
+        <button>Kirim Pesan</button><br><br>
+    </form>
+
+    <a href="index.php">Kembali Ke Menu Utama</a><br>
+</body>
+
+</html>
---
+++ fachri/PHP/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman Utama</title>
+</head>
+
+<body>
+    <h1>Selamat datang di Website Saya</h1>
+
+    <p>Menu :</p>
+
+    <a href="profil.php">Menu Profil</a><br>
+
+    <a href="form.php">Menu Form</a><br>
+
+    <a href="table.php">Menu Table</a><br>
+
+    <a href="list.php">Menu List</a>
+</body>
+
+</html>
